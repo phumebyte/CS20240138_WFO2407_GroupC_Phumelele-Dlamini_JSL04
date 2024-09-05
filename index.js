6a9fa76b3a754b4f6c101cc77765b1df9899da16
phumebyte <phumelele.dlamini1000@gmail.com>
2024-09-05 19:16:28 +0200

    Completed Challenge 4

--- index.js
+++ index.js
@@ -26,8 +26,7 @@ console.log(heartRate < 100 ? "Boost needed!" : "Energy levels are high!");
 // if the temperature is below or equal to 5°C, or "Needs a cooler!" if above 5°C.
 
 let currentTemp = 4; // Current temperature in °C
-console.log(``);
-
+console.log(currentTemp <= 5 ? "Chilled to perfection!" : "Needs a cooler!");
 
 // #### Challenge 5: Late Night Coding Session
 // Determine if it's a good idea to have a Monster Energy drink based on the current hour.
