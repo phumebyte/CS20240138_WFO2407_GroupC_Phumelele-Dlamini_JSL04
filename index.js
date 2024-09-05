9ab917ecb43c29640c04f5d9ec3a9b6f876666ee
phumebyte <phumelele.dlamini1000@gmail.com>
2024-09-05 19:18:03 +0200

    Wrote ternary operator for Challenge 5

--- index.js
+++ index.js
@@ -34,7 +34,8 @@ console.log(currentTemp <= 5 ? "Chilled to perfection!" : "Needs a cooler!");
 // otherwise, print "Better stick to water."
 
 // Define the current hour in 24-hour format
-let currentHour = 22; 
+let currentHour = 22; // Current hour in 24-hour format
+console.log(currentHour >= 7 && currentHour < 24 ? "Unleash the beast!" : "Better stick to water.");
 
 // Write your code below this line to determine if it's a good idea to have a Monster Energy drink based on the current hour
 // If it's between 7am and midnight (24-hour format), print "Unleash the beast!"
